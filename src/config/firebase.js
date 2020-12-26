be2c29e04a604c9e6234aa1356fe8d1b472fe71a
mlobo-dev <mlobo.dev@gmail.com>
2020-12-26 14:48:15 -0300

    Integraçao com o firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,1 +1,13 @@
 import firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: 'AIzaSyCOThbtZcon3Z8fM-HGmxGW27ckn9YZbvA',
+  authDomain: 'eventos-8b39d.firebaseapp.com',
+  projectId: 'eventos-8b39d',
+  storageBucket: 'eventos-8b39d.appspot.com',
+  messagingSenderId: '247786772646',
+  appId: '1:247786772646:web:f17df282644f2a86ec3c67',
+  measurementId: 'G-VCSE7FQW1S',
+};
+// Initialize Firebase
+firebase.analytics();
+export default firebase.initializeApp(firebaseConfig);
